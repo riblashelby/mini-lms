c533ad23757ce4f0ad8ee65f791bc75550397d30
sanDens <ribkashelby@gmail.com>
2026-06-12 18:05:24 +0500

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,12 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 
+const savedTheme = localStorage.getItem('theme')
+
+if (savedTheme) {
+  document.documentElement.dataset.theme = savedTheme
+}
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
